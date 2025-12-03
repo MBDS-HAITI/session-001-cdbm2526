3ddd4df53a520a9f84c8debdc77158678b3fe6a4
cdbm2526 <calebtoussaint@gmail.com>
2025-12-03 08:02:01 -0500

    TD01 : Ajouter un composant pour le contenu principal de la page

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "2rem" }}>
+      <p>Ici, nous afficherons des informations interessantes :)</p>
+    </main>
+  );
+}
+
+export default MainContent;
