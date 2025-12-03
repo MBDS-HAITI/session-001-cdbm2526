fa1052501b22c324a8a0cb2c1ae89d9bf15711f2
cdbm2526 <calebtoussaint@gmail.com>
2025-12-03 08:07:12 -0500

    TD01 : Ajouter un Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ textAlign: "center", marginTop: "3rem", padding: "1rem 0" }}>
+      Tous droits réservés - CDBM2526
+    </footer>
+  );
+}
+
+export default Footer;
